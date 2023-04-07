016fad47a2882532682d238261824f3e373b6b25
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2023-04-07 11:06:25 +0100

    REFACTOR: Refactor the code"

--- js/main.js
+++ js/main.js
@@ -7,5 +7,65 @@ skill_button.addEventListener("click", myFunction);
 function myFunction() {
     skill.style.width = '100%'
     skill.style.display = "flex"
-  
+
+}
+
+//recent works
+
+// fetch works.json 
+function readfromJson() {
+    //write to a json file
+    fetch("./js/works.json")
+        .then(response => response.json())
+        .then(data => {
+            // arquivo JSON foi convertido em um objeto JavaScript
+            //console.log(data.works);
+            //console.log(data.works);
+            listRecent(data.works)
+
+
+        }).catch(error => console.log("Erro ao tentar ler o ficheiro: " + error));
+
+}
+readfromJson()
+
+// get the works data and create a table row for each work
+let recentWorks = document.querySelector('.some_works');
+function listRecent(array) {
+    //console.log(array)
+    recentWorks.innerHTML = "";
+    for (const work of array) {
+        if (work.show_recents === false) {
+            continue;
+        }
+        recentWorks.innerHTML += `
+        <div>
+            <h3>${work.title}</h3>
+            <p>
+                ${work.description}
+            </p>
+
+            <p>Tecnologies: ${work.built_with.join(" • ")}</p>
+            <div class="extern_links">
+            ${show_links(work.link)}
+            </div>
+           
+        </div>
+        `
+    }
+}
+
+//function to show the links to the work
+function show_links(link) {
+    let links = "";
+    const sites = ["site", "git_hub"];
+    sites.forEach((site) => {
+        if (link[site] !== "") {
+            const icon = site === "site" ? "fa-solid fa-arrow-up-right-from-square" : "fa-brands fa-github";
+            links += `<a href="${link[site]}" target="_blank" rel="noopener noreferrer">
+                  <i class="${icon}"></i>
+                </a>`;
+        }
+    });
+    return links;
 }
--- js/works.js
+++ js/works.js
@@ -7,7 +7,7 @@ function readfromJson() {
         .then(response => response.json())
         .then(data => {
             // arquivo JSON foi convertido em um objeto JavaScript
-            console.log(data.works);
+            //console.log(data.works);
             createTable(data.works);
 
 
@@ -22,7 +22,6 @@ var tabela = document.querySelector('table > tbody');
 function createTable(array) {
     tabela.innerHTML = "";
     for (const work of array) {
-        console.log(work);
         tabela.innerHTML += `
         <tr>
             <td>${work.year}</td>
@@ -39,21 +38,24 @@ function createTable(array) {
     }
 }
 
+//function to show the links to the work
 function show_links(link) {
     let links = "";
     const sites = ["site", "git_hub"];
     sites.forEach((site) => {
-      if (link[site] !== "") {
-        const icon = site === "site" ? "fa-solid fa-arrow-up-right-from-square" : "fa-brands fa-github";
-        links += `<a href="${link[site]}" target="_blank" rel="noopener noreferrer">
+        if (link[site] !== "") {
+            const icon = site === "site" ? "fa-solid fa-arrow-up-right-from-square" : "fa-brands fa-github";
+            links += `<a href="${link[site]}" target="_blank" rel="noopener noreferrer">
                   <i class="${icon}"></i>
                 </a>`;
-      }
+        }
     });
     return links;
-  }
-  function show_built_with(array) {
+}
+
+//function to show the built with
+function show_built_with(array) {
     return array.join("<span>·</span>");
-  }
-  
-  
+}
+
+
