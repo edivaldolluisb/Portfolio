7e133f6adb73030f8eddc28d21018539078a925d
Edivaldo Bonfim <edivaldoluisb16@gmail.com>
2023-07-20 00:10:18 +0100

    update

--- js/main.js
+++ js/main.js
@@ -35,15 +35,18 @@ let recentWorks = document.querySelector('.some_works');
 function listRecent(array) {
     const recents = array
       .filter((work) => work.show_recents !== false)
-      .map((work) => `
-        <div>
-          <h3>${work.title}</h3>
-          <p>${work.description}</p>
-          <p>Tecnologies: ${work.built_with.join(" • ")}</p>
-          <div class="extern_links">
-            ${show_links(work.link)}
-          </div>
-        </div>
+      .map((work, index) => `
+               
+        <div class="">
+				<span>0${index+1}</span>
+				<div>
+					<span>0${index+1}</span>
+					<p>${work.title}</p>
+					<span>${work.short_description}</span>
+				</div>
+        ${show_links(work.link)}
+				
+			</div>
       `)
       .join("");
   
@@ -58,9 +61,15 @@ function show_links(link) {
     sites.forEach((site) => {
         if (link[site] !== "") {
             const icon = site === "site" ? "fa-solid fa-arrow-up-right-from-square" : "fa-brands fa-github";
-            links += `<a href="${link[site]}" target="_blank" rel="noopener noreferrer">
-                  <i class="${icon}"></i>
-                </a>`;
+            links += `<a class="button_primary" href="${link[site]}" target="_blank" rel="noopener noreferrer">
+            <span><i class="${icon}"></i></span>
+            <svg width="15px" height="10px" viewBox="0 0 13 10">
+              <path d="M1,5 L11,5"></path>
+              <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+          </a>`;
+
+
         }
     });
     return links;
